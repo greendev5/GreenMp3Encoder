7c9c34427b5faf8b28c880afc4d9c093d801cad4
greendev5 <i.muzichuk@gmail.com>
2017-07-06 13:05:41 +0300

    Removed not needed comments

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,6 @@
 
 int main(int argc, char *argv[])
 {
-//    GMp3Enc::RiffWave w;
-//    w.readWave("./temple_of_love-sisters_of_mercy.wav");
-
-//    if (!w.isValid()) {
-//        return -1;
-//    }
-
-//    GMp3Enc::EncodingTask *t = GMp3Enc::EncodingTask::create(
-//                w, "./1.mp3", 0);
-
-//    t->encode();
-
     GMp3Enc::EncoderApp app(argc, argv);
     return app.exec();
 }
